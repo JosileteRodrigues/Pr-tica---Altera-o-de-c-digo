56f2c64764d5f44fb58b55dee4c44611cc62d054
Josilete Rodrigues <44982021+JosileteRodrigues@users.noreply.github.com>
2023-10-16 10:30:53 -0300

    Update USUARIOS.ts

--- pweb1-Pratica Alterando o Codigo/src/app/shared/model/USUARIOS.ts
+++ pweb1-Pratica Alterando o Codigo/src/app/shared/model/USUARIOS.ts
@@ -1,16 +1,16 @@
 export const USUARIOS = [
   {
-    nome: 'Ryan Nóbrega',
+    nome: 'Maria Clara',
     cpf: "10990106403",
     idade: 19
   },
   {
-    nome: 'Nicolas Nóbrega',
+    nome: 'Josilete',
     cpf: "3883711",
     idade: 15
   },
   {
-    nome: 'Alexandra Nóbrega',
+    nome: 'José Manoel',
     cpf: "198231",
     idade: 50
   }
